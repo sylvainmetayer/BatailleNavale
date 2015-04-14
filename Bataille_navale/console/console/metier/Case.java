728bb57e98fa82c63c770218f24004cce53e4b2e
Syvain Metayer <sylvain.metayer@etu.unilim.fr>
2015-04-14 22:52:02 +0200

    Debut

---
+++ Bataille_navale/console/console/metier/Case.java
@@ -0,0 +1,15 @@
+package console.metier;
+
+public class Case implements Comparable<Case> {
+
+	public Case() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(Case arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
